6a3b5d45cbd7b5bcb47a417abf25ef8f0bf50f2f
goodman2814 <mgoodaman@gmail.com>
2021-02-10 12:33:25 -0500

    Second Commit
    Added file paths and set up problems to solve

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,8 +1,45 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Feb 10 12:14:33 2021
+# ## PyBank
 
-@author: AlexGoodman
-"""
+# ![Revenue](Images/revenue-per-lead.png)
+
+# * In this challenge, you are tasked with creating a Python script for analyzing the financial records of your company. You will give a set of financial data called [budget_data.csv](PyBank/Resources/budget_data.csv). The dataset is composed of two columns: `Date` and `Profit/Losses`. (Thankfully, your company has rather lax standards for accounting so the records are simple.)
+
+# * Your task is to create a Python script that analyzes the records to calculate each of the following:
+
+#   * The total number of months included in the dataset
+
+#   * The net total amount of "Profit/Losses" over the entire period
+
+#   * Calculate the changes in "Profit/Losses" over the entire period, then find the average of those changes
+
+#   * The greatest increase in profits (date and amount) over the entire period
+
+#   * The greatest decrease in losses (date and amount) over the entire period
+
+# * As an example, your analysis should look similar to the one below:
+
+#   ```text
+#   Financial Analysis
+#   ----------------------------
+#   Total Months: 86
+#   Total: $38382578
+#   Average  Change: $-2315.12
+#   Greatest Increase in Profits: Feb-2012 ($1926159)
+#   Greatest Decrease in Profits: Sep-2013 ($-2196167)
+#   ```
+
+# * In addition, your final script should both print the analysis to the terminal and export a text file with the results.
+
+import os
+import csv
+
+
+csvpath = os.path.join('Resources', 'budget_data.csv')
+
+with open(csvpath) as csvfile:
+
+    # CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    print(csvreader)
 
