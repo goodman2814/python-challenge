cca90c36093249a28b5b61a9958fbee6dd082679
goodman2814 <mgoodaman@gmail.com>
2021-02-12 22:40:24 -0500

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,17 +1,3 @@
-# ## PyBank
-
-# ![Revenue](Images/revenue-per-lead.png)
-
-# * In this challenge, you are tasked with creating a Python script for analyzing the financial records of your company. You will give a set of financial data called [budget_data.csv](PyBank/Resources/budget_data.csv). The dataset is composed of two columns: `Date` and `Profit/Losses`. (Thankfully, your company has rather lax standards for accounting so the records are simple.)
-
-# * Your task is to create a Python script that analyzes the records to calculate each of the following:
-
-
-
-#   * The net total amount of "Profit/Losses" over the entire period
-
-#   * Calculate the changes in "Profit/Losses" over the entire period, then find the average of those changes
-
 #   * The greatest increase in profits (date and amount) over the entire period
 
 #   * The greatest decrease in losses (date and amount) over the entire period
@@ -37,50 +23,100 @@ import csv
 csvpath = os.path.join('Resources', 'budget_data.csv')
 
 
-
-
+total_money = []
+month_count = []
+monthly_money = []
 
 with open(csvpath) as csvfile:
 
-    # CSV reader specifies delimiter and variable that holds contents
+# CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
 
-    print(csvreader)
-
-    # Read the header row first (skip this step if there is no header)
+# Read the header row first 
     csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
-    
-    total_money = 0
-    month_count = 0
-    
- #   * The total number of months included in the dataset
+   
+        
+#   * The total number of months included in the dataset
     for row in csvreader:
-        month_count = month_count + 1
+        month_count.append(row[0])
+        total_money.append(int(row[1]))
         
+        total_months = len(month_count)
+
+#   * The net total amount of "Profit/Losses" over the entire period  
+    for i in range(len(total_money)-1):
+        
+#   * Calculate the changes in "Profit/Losses" over the entire period 
+        monthly_money.append(total_money[i+1]-total_money[i])
+  
+        total_amount = sum(total_money)
+#   find the average of those changes
+        avg_change = round(sum(monthly_money)/len(monthly_money), 2)
     
 
-  #   * The net total amount of "Profit/Losses" over the entire period  
-    #for row in csvreader:
-        total_money += int(row[1])
-        
-       
+# grab the max profit
+max_value = max(monthly_money)
     
-#   * Calculate the changes in "Profit/Losses" over the entire period, then find the average of those changes
-        
-        
-print(avg_money)
-    
+# grab the minimum profit
+min_value = min(monthly_money)
+
+
+# find the month associated with the min/max value
+max_month = monthly_money.index(max(monthly_money)) + 1
+min_month = monthly_money.index(min(monthly_money)) + 1
+
+increase_month = month_count[max_month]
+decrease_month = month_count[min_month] 
     
 
+
+print('Financial Analysis')
+print('------------------------------')
+print(f'Total Months: {total_months}')
+print(f'Total: ${total_amount}')
+print(f'Average Change: {avg_change}')
+print(f'Greatest Increase in Profits: {increase_month} {max_value}')
+print(f'Greatest Decrease in Profits: {decrease_month} {min_value}')
+
+# * In addition, your final script should both print the analysis to the terminal and export a text file with the results.
+## WRITE OUTPUT
+# Specify the file to write to
+output_path = os.path.join("Analysis", "Financial_Summary.txt")
+
+# Open the file using "write" mode. Specify the variable to hold the contents
+with open(output_path, 'w') as csvfile:
+
+    csvfile.write('Financial Analysis')
+    csvfile.write("\n")
+    csvfile.write('------------------------------')
+    csvfile.write("\n")
+    csvfile.write(f'Total Months: {total_months}')
+    csvfile.write("\n")
+    csvfile.write(f'Total: ${total_amount}')
+    csvfile.write("\n")
+    csvfile.write(f'Average Change: {avg_change}')
+    csvfile.write("\n")
+    csvfile.write(f'Greatest Increase in Profits: {increase_month} {max_value}')
+    csvfile.write("\n")
+    csvfile.write(f'Greatest Decrease in Profits: {decrease_month} {min_value}')
     
-    
-    
-    
-    
-    
-    
-    
-    
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     
     
